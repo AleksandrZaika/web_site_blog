530b27a502b13bacc04825d8b7f216cb3bde9193
Aleksandr Zaika <zaaleksandr8420@gmail.com>
2021-08-26 01:09:49 +0300

    add initial commit

---
+++ src/main/java/com/example/Web_site_blog/WebSiteBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.Web_site_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSiteBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSiteBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Web_site_blog/WebSiteBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Web_site_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSiteBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
