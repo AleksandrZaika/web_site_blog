8679c55a88a36ee436ffa8a12c1538e583703183
Aleksandr Zaika <zaaleksandr8420@gmail.com>
2021-08-27 00:51:02 +0300

    add home page

--- src/main/java/com/example/Web_site_blog/WebSiteBlogApplication.java
+++ src/main/java/com/example/Web_site_blog/WebSiteBlogApplication.java
@@ -2,8 +2,10 @@ package com.example.Web_site_blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"controllers"})
 public class WebSiteBlogApplication {
 
 	public static void main(String[] args) {
