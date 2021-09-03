6db989743e1693a88caa2b53c00c266b2683ad7a
Aleksandr Zaika <zaaleksandr8420@gmail.com>
2021-09-03 18:08:03 +0300

    add form for adding records

--- src/main/java/com/example/Web_site_blog/models/Post.java
+++ src/main/java/com/example/Web_site_blog/models/Post.java
@@ -1,19 +1,28 @@
 package com.example.Web_site_blog.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Post {
 
-    private Long id;
-    private String title, anons, full_text;
-    private int views;
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String anons;
+    private String full_text;
+    private int views;
+
+    public Post(String title, String anons, String full_text) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
+    }
+
+    public Post() {
+    }
+
     public Long getId() {
         return id;
     }
